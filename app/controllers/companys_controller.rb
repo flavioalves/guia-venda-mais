20ab7b6d9b77add653b61f49c5b064327e201313
Flavio Alves <flaviof.alves@gmail.com>
2013-09-15 19:02:20 -0300

    improve in featured itens presentation

--- app/controllers/companys_controller.rb
+++ app/controllers/companys_controller.rb
@@ -3,21 +3,34 @@ class CompanysController < ApplicationController
   def show
     @categorias = Category.all
     @cidades = City.all 
-    @destaques = Company.includes(:categorys).limit(3)
 
     category_id = params[:category_id]
     city_id = params[:city_id]
 
     if !category_id.blank? && !city_id.blank?
       @empresas = Company.where(' categorys.id = ? AND citys.id = ?', category_id, city_id).includes(:categorys, :citys)
+      @destaques = getFeaturedCompanysByCategory(category_id)
+
     elsif !category_id.blank? and city_id.blank?
       @empresas = Company.where(' categorys.id = ? ', category_id).includes(:categorys, :citys)
+      @destaques = getFeaturedCompanysByCategory(category_id)
+
     elsif category_id.blank? and !city_id.blank?  
       @empresas = Company.where(' citys.id = ?', city_id).includes(:categorys, :citys)
+      @destaques = Company.includes(:categorys).sample(3)
     else
       @empresas = Company.includes(:categorys).all     
-    end
-
-  
+      @destaques = Company.includes(:categorys).sample(3)
+    end  
   end
+
+
+  def getFeaturedCompanysByCategory(category_id)
+    destaques = Company.where(' categorys.id = ? AND featured = ?', category_id, true).includes(:categorys).sample(3).to_a  
+    if destaques.count < 3
+      destaques.concat(Company.where(' featured = ?', true).includes(:categorys).limit(3 - destaques.count))
+    end  
+
+    return destaques
+  end  
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     @categorias = Category.all
     @cidades = City.all 
-    @destaques = Company.includes(:categorys).limit(3)
+    @destaques = Company.where(' featured = ? ', true).includes(:categorys).sample(3)
   end
 
 end
