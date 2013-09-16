90ee404df3f6b7290c6c37b239c401793fe45135
Flavio Alves <flaviof.alves@gmail.com>
2013-09-16 13:31:15 -0300

    add email contact

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,8 @@ class ContactMailer < ActionMailer::Base
 
   def contact_mail(hash)
     @name = hash[:name]
-    @email = hash[:email]
+    #@email = hash[:email]
+    @email = "contato@guiavendamais.com.br"
     @message = hash[:message]
 
     mail(from: @email, subject: "[#{ENV['APP_HOSTNAME']}] #{@name}")
